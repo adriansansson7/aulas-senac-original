b4da65273b05a1f72a89b1c02bc0c79d6a9e2776
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:54:01 -0300

    git-1

--- aula4/index.js
+++ aula4/index.js
@@ -1,4 +1,11 @@
+//olá mundo
 console.log("Hello World!")
+//Primeira execução de variáveis
 const nome = "Ádrian"
 const idade = 19
 console.log("Meu nome é ",nome," e tenho ", idade, "anos de idade")
+//Teste
+const idadeNumero=19
+const idadeTexto=idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
