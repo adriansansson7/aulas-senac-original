238dd98736d1f694bfb37ad44346517513ca6e9d
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-12 09:16:47 -0300

    término-dos-exercícios-de-variáveis

--- aula4/template_javascript/index.js
+++ aula4/template_javascript/index.js
@@ -6,6 +6,24 @@ console.log(typeof idade)*/
 
 // R: Eu não atribui valor na variável //
 
-const nome = "Ádrian"
+/* const nome = "Ádrian"
 const idade = 19
-console.log("Olá!", "Meu nome é ", (nome), "e eu tenho", (idade), "anos")
+console.log("Olá!", "Meu nome é ", (nome), "e eu tenho", (idade), "anos") */
+
+/* let a = 10
+let b = 25
+let c = 0
+console.log("O novo valor de c é", a)
+console.log("O novo valor de a é", b)
+console.log("O novo valor de b é", c) */
+
+let c = 10
+let a = 25
+let b = 0
+console.log("O novo valor de c é", b)
+console.log("O novo valor de b é", c)
+
+/* Os novos valores depois da conversão são:
+a = 25
+b = 10
+c = 0 */
