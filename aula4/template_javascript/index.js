1727829294d1fdfbd6624aecb57a98703f45c494
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-15 09:37:44 -0300

    Testando commit

--- aula4/template_javascript/index.js
+++ aula4/template_javascript/index.js
@@ -1,29 +1,24 @@
-/*let nome
+//Recebeu as variáveis
+let nome = prompt("Digite seu nome:")
+let idade = prompt("Digite usa idade")
+//MENSAGEM: Digite as horas que você trabalha por dia:
+let horasTrabalho = prompt("Digite as horas que você trabalha por dia")
+let salarioDia = prompt("Digite o seu salário diário")
+//Verificando as variáveis
 console.log(typeof nome)
-
-let idade
-console.log(typeof idade)*/
-
-// R: Eu não atribui valor na variável //
-
-/* const nome = "Ádrian"
-const idade = 19
-console.log("Olá!", "Meu nome é ", (nome), "e eu tenho", (idade), "anos") */
-
-/* let a = 10
-let b = 25
-let c = 0
-console.log("O novo valor de c é", a)
-console.log("O novo valor de a é", b)
-console.log("O novo valor de b é", c) */
-
-let c = 10
-let a = 25
-let b = 0
-console.log("O novo valor de c é", b)
-console.log("O novo valor de b é", c)
-
-/* Os novos valores depois da conversão são:
-a = 25
-b = 10
-c = 0 */
+console.log(typeof horasTrabalho)
+console.log(typeof salarioDia)
+//Convertendo o que é necessário
+let horasTrabNumber = Number(horasTrabalho)
+let salDiaNumber = Number(salarioDia)
+//Processando valores
+let valHoraNumber = horasTrabNumber/salDiaNumber
+//Conversão para não dar erro de NULL
+let valorHora = String(valHoraNumber)
+const mensagem = "Seu valor hora e " + valorHora + "!!!"
+//Verificando variáveis
+console.log(typeof valorHora)
+console.log(typeof mensagem)
+console.log(mensagem)
+//Saída de valores para Front End
+alert(mensagem)
