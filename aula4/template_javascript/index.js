31fcf43eebd8ab8338a9d34ca175396838480692
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:55:22 -0300

    portas-logicas

--- aula4/template_javascript/index.js
+++ aula4/template_javascript/index.js
@@ -1,24 +1,192 @@
-//Recebeu as variáveis
-let nome = prompt("Digite seu nome:")
-let idade = prompt("Digite usa idade")
-//MENSAGEM: Digite as horas que você trabalha por dia:
-let horasTrabalho = prompt("Digite as horas que você trabalha por dia")
-let salarioDia = prompt("Digite o seu salário diário")
-//Verificando as variáveis
-console.log(typeof nome)
-console.log(typeof horasTrabalho)
-console.log(typeof salarioDia)
-//Convertendo o que é necessário
-let horasTrabNumber = Number(horasTrabalho)
-let salDiaNumber = Number(salarioDia)
-//Processando valores
-let valHoraNumber = horasTrabNumber/salDiaNumber
-//Conversão para não dar erro de NULL
-let valorHora = String(valHoraNumber)
-const mensagem = "Seu valor hora e " + valorHora + "!!!"
-//Verificando variáveis
-console.log(typeof valorHora)
-console.log(typeof mensagem)
-console.log(mensagem)
-//Saída de valores para Front End
-alert(mensagem)
+/* let resultado
+
+resultado = 3 + 4
+alert(resultado)
+
+resultado = (3+5)/2
+alert(resultado)
+
+resultado = 3+5/2
+alert(resultado)
+
+resultado = (4-5)*-1
+alert(resultado)
+
+resultado = 234 % 5
+alert(resultado)
+
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Testes de comparação
+resultado = primeiroNumero === segundoNumero
+
+//Mostra resultado na tela
+alert(resultado)
+
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Testes de comparação
+resultado = primeiroNumero !== segundoNumero
+
+//Mostra resultado na tela
+alert(resultado)
+
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Testes de comparação
+resultado = primeiroNumero >= segundoNumero
+
+//Mostra resultado na tela
+alert(resultado)
+
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//Leitura de dados informados pelo usuário
+primeiroNumero = prompt("Digite o primeiro numero:")
+segundoNumero = prompt("Digite o segundo numero:")
+
+//Testes de comparação
+resultado = primeiroNumero <= segundoNumero
+
+//Mostra resultado na tela
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a && b
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = b && c
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a && c
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a && b && c
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a || b
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = b || c
+alert(resultado)
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a || c
+alert(resultado) */
+
+let a
+let b
+let c
+let resultado
+
+//Leitura de dados informados pelo usuário
+a = true
+b = false
+c = true
+
+
+//Testes de comparação
+resultado = a || b || c
+alert(resultado)
