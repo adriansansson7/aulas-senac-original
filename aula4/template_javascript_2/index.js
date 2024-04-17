b7facb291a4a875af1c2dbc5ebc84aadca089b89
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 11:52:03 -0300

    Atividade_de_escrita_code3

--- aula4/template_javascript_2/index.js
+++ aula4/template_javascript_2/index.js
@@ -1,4 +1,4 @@
-/* et idade = prompt("Qual é sua idade?")
+/* let idade = prompt("Qual é sua idade?")
 
 let idadeBest = prompt("Qual é sua idade?")
 
@@ -7,18 +7,30 @@ let comparação = idade >= idadeBest
 console.log("Sua idade é maior do que a do seu melhor amigo?" , (comparação))
 
 let diferençaIdade = idade - idadeBest
-console.log("A diferença de idade é de?" , (diferençaIdade)) */
+console.log("A diferença de idade é de?" , (diferençaIdade))
 
 let numeroPar = Number(prompt("Digite um número par:"))
 
 console.log(numeroPar % 2)
-
-let numeroPar2 = Number(prompt("Digite um número par:"))
-
-console.log(numeroPar2 % 2)
 
 // Sim, todos os número pares dividos por 2, resultam em 0.
 
 let numeroImpar = Number(prompt("Digite um número impar:"))
 
 console.log(numeroImpar % 2)
+
+// Sim, quando o número é impar, sobra o resto da divisão */
+
+let idadeAnos = prompt("Qual é sua idade?")
+
+let idadeMeses = idadeAnos * 12
+alert(idadeMeses)
+console.log(idadeAnos * 12)
+
+let idadeDias = idadeAnos * 365
+alert(idadeDias)
+console.log(idadeAnos * 365)
+
+let idadeHoras = idadeAnos * 8786
+alert(idadeHoras)
+console.log(idadeAnos * 8786)
