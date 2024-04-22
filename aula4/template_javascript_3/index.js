eda0e2277ebdb4882846aec3277801c7fd24a3fc
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-22 11:55:33 -0300

    atividade_22-04

---
+++ aula4/template_javascript_3/index.js
@@ -0,0 +1,11 @@
+/* const nomeDoUsuario = prompt("Digite seu nome:")
+const emailDoUsuario = prompt("Digite seu e-mail:")
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeDoUsuario}!`)
+
+let arrayComidas = ["pizza", "lasanha", "panqueca", "sushi", "lentilha"]
+console.log(arrayComidas)
+console.log("Essas são as minhas comidas preferidas:")
+console.log(arrayComidas[0] + "\n" + arrayComidas[1] + "\n" + arrayComidas[2] + "\n" + arrayComidas[3] + "\n" + arrayComidas[4])
+const faleComida = prompt("Digite uma comida preferida:")
+arrayComidas[2] = faleComida
+console.log(arrayComidas) */
