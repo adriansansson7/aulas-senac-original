bf3b824be1225124dcdc872d8b77ba28bffdbf10
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 11:50:16 -0300

    aula-23_04

--- aula4/template_javascript_2/index.js
+++ aula4/template_javascript_2/index.js
@@ -1,92 +1,62 @@
-/* let idade = prompt("Qual é sua idade?")
+/* let num1
+let num2
 
-let idadeBest = prompt("Qual é sua idade?")
-
-let comparação = idade >= idadeBest
-
-console.log("Sua idade é maior do que a do seu melhor amigo?" , (comparação))
-
-let diferençaIdade = idade - idadeBest
-console.log("A diferença de idade é de?" , (diferençaIdade))
-
-let numeroPar = Number(prompt("Digite um número par:"))
-
-console.log(numeroPar % 2)
-
-// Sim, todos os número pares dividos por 2, resultam em 0.
-
-let numeroImpar = Number(prompt("Digite um número impar:"))
-
-console.log(numeroImpar % 2)
-
-// Sim, quando o número é impar, sobra o resto da divisão
-
-let idadeAnos = prompt("Qual é sua idade?")
-
-let idadeMeses = idadeAnos * 12
-alert(idadeMeses)
-console.log(idadeAnos * 12)
-
-let idadeDias = idadeAnos * 365
-alert(idadeDias)
-console.log(idadeAnos * 365)
-
-let idadeHoras = idadeAnos * 8786
-alert(idadeHoras)
-console.log(idadeAnos * 8786)
-
-let num1 = Number (prompt("Digite um número:"))
-let num2= Number (prompt("Digite outro número:"))
-console.log(`O primeiro número é maior que o segundo? ${num1 >= num2}`)
-console.log(`O primeiro numero é igual ao segundo? ${num1 >= num2}`)
-let divC = (0)
-let divD = (0)
-let respostaC = (num1 % num2)
-let respostaD = (num2 % num1)
-console.log(`O primeiro numero é divisível pelo segundo? ${divC === respostaC}`)
-console.log(`O segundo numero é divisível pelo primeiro? ${divD === respostaD}`)
-
-let nome = prompt("Digite seu nome:")
-let cor = prompt("Digite sua cor favorita:")
-console.log("A cor favorita de " + nome + " é " + cor)
-console.log(`A cor favorita de ${nome} é ${cor}`)
-
-const frase = prompt("Digite uma frase:")
-alert(frase)
-const fraseMaiuscula = (frase.toUpperCase())
-alert(fraseMaiuscula)
-const fraseVogais = (fraseMaiuscula.replaceAll("O" , "I"))
-alert(fraseVogais)
-const fraseCaract = (fraseMaiuscula.length)
-alert(fraseCaract)
-
-const adocaoDog = ["Pincher" , "Golden Retriver" , "Buldogue" , "Pastor Alemão" , "Dachshund"]
-const escolhido = prompt("Digite um número de 0 a 4")
-alert(adocaoDog[escolhido])
-
-const valoresIniciais = ["1" , "2" , "3" , "4" , "5" , "6"]
-console.log(valoresIniciais)
-console.log(valoresIniciais.length)
-console.log["1" , "2" , "3" , "4" , "5" , "6"]
-valoresIniciais.push(7)
-console.log(valoresIniciais)
-valoresIniciais.splice(4, 5)
-console.log(valoresIniciais)
-console.log(valoresIniciais.length)
-
-let listaDeTarefas = []
-
-for (let i = 0; i < 3; i++) {
-let tarefa = prompt("Digite uma tarefa que você precise realizar hoje:")
-listaDeTarefas.push(tarefa)
+num1 = prompt("Digite um número:")
+num2 = prompt("Digite um número:")
+if (num1 === num2) {
+    alert(`Números iguais!`)
 }
-console.log("Lista de tarefas:")
-console.log(listaDeTarefas)
 
-let indiceRealizada = parseInt(prompt("Digite o índice da tarefa que você já realizou (0, 1 ou 2):"))
+let maiorIdade = []
+let idade = Number(prompt("Digite sua idade:"))
+maiorIdade.push(idade)
 
-if (indiceRealizada >= 0 && indiceRealizada < listaDeTarefas.length)
-listaDeTarefas.splice(indiceRealizada, 1)
-console.log("\n Tarefa removida:")
+let idadeMinima = 18
+switch (maiorIdade && idadeMinima) {
 
-console.log(listaDeTarefas) */
+    case 'maiorIdade === idadeMinima':
+        console.log('Você pode dirigir!')
+        break
+    case 'maiorIdade || idadeMinima':
+        console.log('Você pode dirigir!')
+        break
+    case 'maiorIdade < idadeMinima':
+        console.log('Você não pode dirigir!')
+        break
+} 
+
+let maiorIdade = []
+let idade = Number(prompt("Digite sua idade:"))
+maiorIdade.push(idade)
+
+if (maiorIdade === '18') {
+    console.log('Você pode dirigir')
+} else if (maiorIdade >= '18'){
+    console.log('Você pode dirigir')
+} else if (maiorIdade <= '18'){
+    console.log('Você não pode dirigir')
+} 
+
+let turno = prompt("Digite o turno que você estuda: Manhã, Tarde ou Noite:")
+
+if (turno === 'Manhã') {
+    console.log('Bom Dia!!')
+} else if (turno === 'Tarde'){
+    console.log('Boa Tarde!!')
+} else if (turno === 'Noite'){
+    console.log('Boa Noite!!')
+}
+
+let turno = prompt("Digite o turno que você estuda: Manhã, Tarde ou Noite:")
+
+switch (turno) {
+    case 'Manhã':
+        console.log('Bom Dia!!')
+        break
+    case 'Tarde':
+        console.log('Boa Tarde!!')
+        break
+    case 'Noite':
+    console.log('Boa Noite!!')
+    break
+} */
