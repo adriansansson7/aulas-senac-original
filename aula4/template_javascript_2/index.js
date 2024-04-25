365ad8bfe772dc47436164683aee05297134c4a1
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-25 11:42:27 -0300

    Término_da_aula_24-04

--- aula4/template_javascript_2/index.js
+++ aula4/template_javascript_2/index.js
@@ -68,15 +68,70 @@ if (genero === 'Fantasia' && preço <= 15) {
     console.log('Bom Filme!!')
 } else {
     console.log("Escolha outro filme :(")
-} */
+}
 
-let pets = []
-const nomes = []
-let quantos = Number (prompt("Você tem quantos pets?"))
-pets.push(quantos)
-let pet = 0
+let quantidadePets
+quantidadePets = Number(prompt('Quantos bichinhos de estimação você tem?'))
 
+if (quantidadePets === 0) {
+    alert('Que pena! Você pode adotar um pet!');
+} else {
+    let i = 0
+    let nomesPets = []
+    for (i= 0; i < quantidadePets; i++) {
+        let nomePet = prompt(`Digite o nome do ${i+1}º pet:`)
+        nomesPets.push(nomePet)
+    }
 
-if (pets == 0) {
-        alert('Que pena! Você pode adotar um pet!')
-} else if (pets > 0)
+    console.log('Nomes dos pets:');
+    for (let nome of nomesPets) {
+        console.log(nome);
+    }
+}
+
+let arrayOriginal
+arrayOriginal = [7 , 12 , 2, 7777, 77, 777, 9]
+console.log("Valores do array original:")
+for (let valor of arrayOriginal) {
+    console.log(valor)
+}
+console.log("\n" + "Valores do array original divididos por 10:")
+for (let valor of arrayOriginal) {
+    console.log(valor / 10)
+}
+let numerosPares = []
+for (let valor of arrayOriginal) {
+    if (valor % 2 === 0) {
+        numerosPares.push(valor)
+    }
+}
+console.log("\n" + "Números pares do array original:")
+console.log(numerosPares)
+let arrayStrings = []
+for (let i = 0; i < arrayOriginal.length; i++) {
+    arrayStrings.push(`O elemento do índex ${i} é: ${arrayOriginal[i]}`)
+}
+console.log("\n" + "Array de strings formatadas:")
+console.log(arrayStrings)
+let maiorNumero = arrayOriginal[0]
+let menorNumero = arrayOriginal[0]
+for (const valor of arrayOriginal) {
+    if (valor > maiorNumero) {
+        maiorNumero = valor
+    }
+}
+console.log("\n" + "Maior número do array original:", + maiorNumero)
+console.log("Menor número do array original:", + menorNumero) */
+
+let filme = {
+    nome: 'One Piece' ,
+    diretor: 'Eiichiro Oda' ,
+    lançamento: '1997' ,
+    elenco: 'Luffy, Zoro, Usopp, Sanji, Nami, Chopper, Robin, Franky, Brook e Jinbe' ,
+    javi: 'SIM'
+}
+alert(filme.nome)
+alert(filme['diretor'])
+alert(filme.lançamento)
+alert(filme['elenco'])
+alert(filme.javi)
