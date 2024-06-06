1a59bc78dcb594fd0e613e689dc9e5971cc12dd5
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-06-06 11:37:45 -0300

    Trabalho-de-SoftSkills

--- aula4/template_javascript_2/index.js
+++ aula4/template_javascript_2/index.js
@@ -264,7 +264,7 @@ function main() {
     }
 }
 
-main() */
+main()
 
 //criei um objeto vazio
 
@@ -328,8 +328,9 @@ let numDoadores = 1
 function doadores() {
     let mensagem = ""
     mensagem += "LISTA DE DOADORES \n"
-    for (let i = 0; i < numDoadores; i++){
-alert(mensagem + '\n' + `${nome}, ${idade}, ${peso}Kg, ${fatorRh}, ${data}` + '\n' + `${nome1}`)
+    for (let i = 0; i < numDoadores.length; i++){
+        const unicoDoador = numDoadores[1]
+        alert(mensagem + '\n' + `${i+1} de ${numDoadores.length} \nNome: ${unicoDoador.nome} \nIdade: ${unicoDoador.idade} \nPeso: ${unicoDoador.peso}Kg \nTipo Sanguíneo: ${unicoDoador.fatorRh} \nData da Última Doação: ${unicoDoador.data} \n ${nome1}`)
     }
 }
 
@@ -409,6 +410,209 @@ function main() {
                 break
             case 4:
                 buscarDois()
+                break
+        }
+    }
+}
+
+main()
+
+const desastresNaturais = ["terremoto", "tsunami", "furacão", "inundação", "erupção vulcânica", "guerra", "incêndio", "roubo"]
+
+let armazenar1 = []
+let armazenar2 = []
+
+function imprimeMenu(){
+    let mensagem = ""
+    mensagem += "********************************************************************" + "\n"
+    mensagem += "DIGITE UM DOS NUMEROS ABAIXO, CONFORME SUA ESCOLHA:" + "\n"
+    mensagem += "********************************************************************" + "\n"
+    mensagem += "1- Desastre" + "\n"
+    mensagem += "2- Local" + "\n"
+    mensagem += "3- Alertar" + "\n"
+    mensagem += "4- Sair" + "\n"
+    mensagem += "*******************************************************************"
+    let opcao = Number(prompt(mensagem))
+    return opcao
+}
+
+function desastre() {
+    let mensagem = ""
+    mensagem += ("SELECIONE UM NÚMERO DE 1 A 8 PARA ESCOLHER O DESASTRE/CALAMIDADE \n 1- Terremoto \n 2- Tsunami \n 3- Furacão \n 4- Inundação \n 5- Erupção Vulcânica \n 6- Guerra \n 7- Incêndio \n 8- Roubo")
+    opcoes1 = prompt(mensagem)
+    switch (opcoes1) {
+        case "1":
+            armazenar1.push(`${desastresNaturais}`[0])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[0].toUpperCase()}`)
+            break
+        case "2":
+            armazenar1.push(`${desastresNaturais}`[1])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[1].toUpperCase()}`)
+            break
+        case "3":
+            armazenar1.push(`${desastresNaturais}`[2])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[2].toUpperCase()}`)
+            break
+        case "4":
+            armazenar1.push(`${desastresNaturais}`[3])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[3].toUpperCase()}`)
+            break
+        case "5":
+            armazenar1.push(`${desastresNaturais}`[4])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[4].toUpperCase()}`)
+            break
+        case "6":
+            armazenar1.push(`${desastresNaturais}`[5])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[5].toUpperCase()}`)
+            break
+        case "7":
+            armazenar1.push(`${desastresNaturais}`[6])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[6].toUpperCase()}`)
+            break
+        case "8":
+            armazenar1.push(`${desastresNaturais}`[7])
+            alert(`ALERTA! Ocorreu um ${desastresNaturais[7].toUpperCase()}`)
+            break
+    }
+}
+
+const locais = ["NO PAÍS", "NO ESTADO", "NA CIDADE", "NAS PROXIMIDADES"]
+
+function local() {
+    let mensagem = ""
+    mensagem += ("SELECIONE UM NÚMERO DE 1 A 4 PARA ESCOLHER O LOCAL QUE VOCÊ QUER ALERTAR \n 1- País \n 2- Estado \n 3- Cidade \n 4- Proximidades")
+    opcoes2 = prompt(mensagem)
+    switch (opcoes2) {
+        case "1":
+            armazenar2.push(`${locais}`[0])
+            alert(`DESASTRE/CALAMIDADE ${locais[0].toUpperCase()}`)
+            break
+        case "2":
+            armazenar2.push(`${locais}`[1])
+            alert(`DESASTRE/CALAMIDADE ${locais[1].toUpperCase()}`)
+            break
+        case "3":
+            armazenar2.push(`${locais}`[2])
+            alert(`DESASTRE/CALAMIDADE ${locais[2].toUpperCase()}`)
+            break
+        case "4":
+            armazenar2.push(`${locais}`[3])
+            alert(`DESASTRE/CALAMIDADE ${locais[3].toUpperCase()}`)
+    }
+}
+
+function alertarUrgente() {
+    mensagem += "ALERTA!!!!!\n";
+    mensagem += `Desastre: ${armazenar1.join(", ")}`
+    mensagem += `Local: ${armazenar2.join(", ")}`
+    alert(mensagem);
+}
+
+function sair() {
+    alert("Saindo do programa.");
+}
+
+function main() {
+    let option = 0
+    while(option !== 5){
+        option = imprimeMenu()
+        switch (option) {
+            case 1:
+                desastre()
+                break
+            case 2:
+                local()
+                break
+            case 3:
+                alertarUrgente()
+                break
+            case 4:
+                sair()
+                break
+        }
+    }
+}
+
+main() */
+
+const desastresNaturais = ["Terremoto", "tsunami", "furacão", "inundação", "erupção vulcânica", "guerra", "incêndio", "roubo"]
+let armazenar1 = []
+let armazenar2 = []
+
+function imprimeMenu(){
+    let mensagem = ""
+    mensagem += "********************************************************************" + "\n"
+    mensagem += "DIGITE UM DOS NÚMEROS ABAIXO, CONFORME SUA ESCOLHA:" + "\n"
+    mensagem += "********************************************************************" + "\n"
+    mensagem += "1- Desastre" + "\n"
+    mensagem += "2- Local" + "\n"
+    mensagem += "3- Alertar" + "\n"
+    mensagem += "4- Sair" + "\n"
+    mensagem += "*******************************************************************"
+    let opcao = Number(prompt(mensagem))
+    return opcao
+}
+
+function desastre() {
+    let mensagem = ""
+    mensagem += "SELECIONE UM NÚMERO DE 1 A 8 PARA ESCOLHER O DESASTRE/CALAMIDADE\n"
+    desastresNaturais.forEach((desastre, index) => {
+        mensagem += `${index + 1}- ${desastre}\n`
+    })
+    let opcoes1 = Number(prompt(mensagem))
+    if (opcoes1 >= 1 && opcoes1 <= desastresNaturais.length) {
+        armazenar1.push(desastresNaturais[opcoes1 - 1])
+        alert(`ALERTA! Ocorreu um ${desastresNaturais[opcoes1 - 1].toUpperCase()}`)
+    } else {
+        alert("Opção inválida. Por favor, escolha um número válido.")
+    }
+}
+
+const locais = ["no País", "no Estado", "na Cidade", "nas Proximidades"]
+
+function local() {
+    let mensagem = ""
+    mensagem += "SELECIONE UM NÚMERO DE 1 A 4 PARA ESCOLHER O LOCAL QUE VOCÊ QUER ALERTAR\n"
+    locais.forEach((local, index) => {
+        mensagem += `${index + 1}- ${local}\n`
+    })
+    let opcoes2 = Number(prompt(mensagem))
+    if (opcoes2 >= 1 && opcoes2 <= locais.length) {
+        armazenar2.push(locais[opcoes2 - 1])
+        alert(`DESASTRE/CALAMIDADE ${locais[opcoes2 - 1].toUpperCase()}`)
+    } else {
+        alert("Opção inválida. Por favor, escolha um número válido.")
+    }
+}
+
+function alertarUrgente() {
+    let mensagem = ""
+    mensagem += "ALERTA!!!!!\n"
+    mensagem += `DESASTRE/CALAMIDADE: ${armazenar1.join(", ")}\n`
+    mensagem += `LOCAL:: ${armazenar2.join(", ")}`
+    alert(mensagem);
+}
+
+function sair() {
+    alert("Saindo do programa.")
+}
+
+function main() {
+    let option = 0
+    while(option !== 4){
+        option = imprimeMenu()
+        switch (option) {
+            case 1:
+                desastre()
+                break
+            case 2:
+                local()
+                break
+            case 3:
+                alertarUrgente()
+                break
+            case 4:
+                sair()
                 break
         }
     }
